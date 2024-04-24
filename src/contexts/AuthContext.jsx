5784854bec3d93d83dd6e6e1d2a23080b1f1dce2
Oleksandr <skairail1600@gmail.com>
2024-04-24 20:29:43 +0300

    auth context updated

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useReducer } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AuthContext = createContext();
 
@@ -10,7 +11,7 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "login":
-      return { ...state, user: action.payload, isAuthenticated: true };
+      return { ...state, user: action.payload.user, isAuthenticated: true };
     case "logout":
       return { ...state, user: null, isAuthenticated: false };
     default:
@@ -37,18 +38,39 @@ function AuthProvider({ children }) {
       const data = await response.json();
 
       if (response.ok) {
-        dispatch({ type: "login", payload: data.user });
+        dispatch({ type: "login", payload: data });
+        saveToken(data.accessToken);
+        return data.accessToken;
       } else {
         console.error(data.error);
+        return null;
       }
     } catch (error) {
       console.error("Error during login:", error);
+      return null;
     }
   }
 
   function logout() {
     dispatch({ type: "logout" });
+    removeToken();
   }
+
+  const saveToken = async (token) => {
+    try {
+      await AsyncStorage.setItem("authToken", token);
+    } catch (error) {
+      console.error("Error saving token:", error);
+    }
+  };
+
+  const removeToken = async () => {
+    try {
+      await AsyncStorage.removeItem("authToken");
+    } catch (error) {
+      console.error("Error when deleting token:", error);
+    }
+  };
 
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
